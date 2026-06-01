e996c7356980c224bb8f11298b0170fd88dc9df0
Mithuna Raj <mithunarj123@gmail.com>
2026-06-01 13:37:04 +0000

    Implemented User Creation Successfully

--- scripts/user_management.sh
+++ scripts/user_management.sh
@@ -4,7 +4,7 @@ echo "    "
 read -p "Enter Username: " username
 if  id "$username" >> /dev/null 2>&1; then
         echo "User exists"
-	User_details=$(id $username)
+	User_details=$(id "$username")
 	echo "User details: $User_details"
 else
         echo "User doesn't exist"
@@ -14,3 +14,17 @@ echo "    "
 User_List=$(cut -d : -f 1 /etc/passwd)
 echo "Users_List:" 
 echo "$User_List"
+read -p " Enter New User name  to create a user:" New_User
+if id "$New_User" > /dev/null 2>&1; then
+	echo " User already exist"
+else
+	sudo useradd -m $New_User
+	if [ $? -eq 0 ]; then
+		echo "User Created Successfully"
+	        New_User_Details=$(id "$New_User")
+	        echo "New User Details: $New_User_Details"
+	else
+		echo "User creation failed"
+	fi
+fi
+
